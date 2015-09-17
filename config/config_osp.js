78f9db37258873208a7e4c37eb8208ab1d4a6f40
Sergey Sapon <svs2022@ukr.net>
2015-09-17 12:54:12 +0300

    201509171254:v.0.0.2

---
+++ config/config_osp.js
@@ -0,0 +1,7 @@
+var fbcon = {};
+fbcon.host = '192.168.40.200';
+fbcon.port = '3050';
+fbcon.database = '\\database\\zp_osp.fdb';
+fbcon.user = 'SYSDBA';
+fbcon.password = 'masterkey';
+var port = 3000;
